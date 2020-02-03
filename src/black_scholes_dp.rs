328d006b311ff183de9d2061c839e9520f28d7f5
Henri Lunnikivi <henri.lunnikivi@gmail.com>
2020-02-03 20:17:17 +0200

    Initialized Rust-project and copied relevant transpiled sources

---
+++ src/black_scholes_dp.rs
@@ -0,0 +1,39 @@
+use std::collections::HashMap;
+use std::*;
+
+use numpy::float64;
+fn black_scholes_call_div(S: i32, K: i32, T: i32, r: f32, q: f32, sigma: f32) -> float64 {
+    let d1 = np.log(S / K) + r - q + 0.5 * sigma.pow(2) * T / sigma * np.sqrt(T);
+    let d2 = np.log(S / K) + r - q - 0.5 * sigma.pow(2) * T / sigma * np.sqrt(T);
+    let call = S * np.exp(-(q) * T) * si.norm.cdf(d1, 0.0, 1.0)
+        - K * np.exp(-(r) * T) * si.norm.cdf(d2, 0.0, 1.0);
+    return call;
+}
+fn black_scholes_put_div(S: i32, K: i32, T: i32, r: f32, q: f32, sigma: f32) -> float64 {
+    let d1 = np.log(S / K) + r - q + 0.5 * sigma.pow(2) * T / sigma * np.sqrt(T);
+    let d2 = np.log(S / K) + r - q - 0.5 * sigma.pow(2) * T / sigma * np.sqrt(T);
+    let put = K * np.exp(-(r) * T) * si.norm.cdf(-(d2), 0.0, 1.0)
+        - S * np.exp(-(q) * T) * si.norm.cdf(-(d1), 0.0, 1.0);
+    return put;
+}
+fn euro_vanilla_dividend(
+    S: i32,
+    K: i32,
+    T: i32,
+    r: f32,
+    q: f32,
+    sigma: f32,
+    option: &str,
+) -> float64 {
+    let d1 = np.log(S / K) + r - q + 0.5 * sigma.pow(2) * T / sigma * np.sqrt(T);
+    let d2 = np.log(S / K) + r - q - 0.5 * sigma.pow(2) * T / sigma * np.sqrt(T);
+    if option == "call" {
+        let mut result = S * np.exp(-(q) * T) * si.norm.cdf(d1, 0.0, 1.0)
+            - K * np.exp(-(r) * T) * si.norm.cdf(d2, 0.0, 1.0);
+    }
+    if option == "put" {
+        let mut result = K * np.exp(-(r) * T) * si.norm.cdf(-(d2), 0.0, 1.0)
+            - S * np.exp(-(q) * T) * si.norm.cdf(-(d1), 0.0, 1.0);
+    }
+    return result;
+}
---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use std::collections::HashMap;
+use std::*;
+
+use black_scholes_dp::*;
+use black_scholes_ndp::*;
+fn main() {
+    euro_vanilla_call(50, 100, 1, 0.05, 0.25);
+    euro_vanilla_put(50, 100, 1, 0.05, 0.25);
+    euro_vanilla(50, 100, 1, 0.05, 0.25, "put");
+    black_scholes_call_div(50, 100, 1, 0.05, 0.06, 0.25);
+    black_scholes_put_div(50, 100, 1, 0.05, 0.06, 0.25);
+    euro_vanilla_dividend(50, 100, 1, 0.05, 0.06, 0.25, "put");
+}
